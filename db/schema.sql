833d01ae0d7cf8bb12c7edbbdbe5d854449daf0f
Nate Naranjo <naranjo_nathaniel@live.com>
2022-09-26 18:46:56 -0500

    Created the connection file and database schema file... Setup the server js file and added the secret for sessions to the env file.

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS codingblog_db;
+CREATE DATABASE codingblog_db;
--- server.js
+++ server.js
@@ -1,3 +1,44 @@
-console.log("Nodemon is working correctly");
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('expire-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utilis/ helpers');
 
-console.log("Mrs. Megan is awesome")
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create(( helpers ));
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('NOw listening'));
+});
