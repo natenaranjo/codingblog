0de9e804641314418ba3d8832a0e85eb553a2ad3
Nate Naranjo <naranjo_nathaniel@live.com>
2022-09-28 14:42:58 -0500

    Created the main index, homeRoutes files for the controller

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('login', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
