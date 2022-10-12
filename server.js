9b6575856ae0de087b63283170f9bbbdfc03300a
Nate Naranjo <naranjo_nathaniel@live.com>
2022-10-12 14:57:19 -0500

    changed the exphbs create on server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ defaultLayout: "main", extname: '.hbs', helpers: helpers });
+const hbs = exphbs.create({ });
 
 const sess = {
     secret: "Hackers 1995",
